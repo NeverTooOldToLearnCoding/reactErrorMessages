2449cd003095c120dcd5066f9ec45a58ebd50b5c
Daniel Lam <danlam@ucdavis.edu>
2019-02-28 22:41:43 -0800

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 
@@ -80,17 +80,11 @@ class App extends Component {
 
   render() {
 
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
 
-    };
 
     let persons = null;
+    let btnClass = '';
+
 
     //output content conditionally by using javascript and the same for lists
     //can be done with ternary expressions using ? and null operators
@@ -111,29 +105,29 @@ class App extends Component {
         </div>
       );
 
-      style.backgroundColor = 'red';
-
+      btnClass = classes.Red;
 
     }
 
-    const classes = [];
+    const assignedClasses = [];
+
     if (this.state.persons.length < 3) {
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
 
     if (this.state.persons.length < 2) {
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
     return (
 
-        <div className="App">
+        <div className={classes.App}>
 
           <h1>Hi, I'm a React App</h1>
-          <p className={classes.join(' ')}>THis is really working!</p>
+          <p className={assignedClasses.join(' ')}>THis is really working!</p>
 
           <button
-            style={style}
+            className = {btnClass}
             onClick={this.togglePersonsHandler}>Toggle Person
         </button>
 
